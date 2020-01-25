3a681ac171782df8d0259403ad6d093a6981cd97
Brad Davis <braddavis97@gmail.com>
2020-01-25 18:12:48 -0500

    Working on the controller

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,5 +1,33 @@
-const socket = io.connect();
+const socket = io();
+let invert = false;
+window.addEventListener("deviceorientation", handleOrientation, true);
+const leftTri = document.getElementsByClassName("triangle-left")[0];
+const rightTri = document.getElementsByClassName("triangle-right")[0];
+const upTri = document.getElementsByClassName("triangle-up")[0];
 
-function main() {
-  socket.emit();
+function invertControls() {
+  invert = !invert;
+}
+
+function handleOrientation(event) {
+  //gamma is accelleration as gamma increases
+  //beta is lef
+  if (!invert) {
+    const out = {
+      turn: event.beta,
+      speed: event.gamma
+    };
+    socket.emit("phone", out);
+  } else {
+    const out = {
+      turn: -event.beta,
+      speed: event.gamma
+    };
+    socket.emit("phone", out);
+  }
+  leftTri.style.borderLeft = "50px solid # ";
+  rightTri.style.borderLeft = "50px solid # ";
+  upTri.style.borderLeft = "50px solid # ";
+
+  // Do stuff with the new orientation data
 }
